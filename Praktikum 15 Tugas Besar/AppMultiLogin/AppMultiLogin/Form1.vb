048a35e63b31a8295b788c84d6fe083d14e10eb0
juhansyahtribs <103118239+juhansyahtribs@users.noreply.github.com>
2023-07-25 01:41:49 +0700

    Add files via upload

---
+++ Praktikum 15 Tugas Besar/AppMultiLogin/AppMultiLogin/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Private Sub Admin1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Admin1ToolStripMenuItem.Click
+        FormDatapengguna.ShowDialog()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MatikanSemuaMenu()
+
+    End Sub
+
+    Private Sub MatikanSemuaMenu()
+        MenuAdminBar.Visible = False
+        LoginBar.Visible = True
+        LogoutBar.Visible = False
+    End Sub
+
+    Private Sub LoginBar_Click(sender As Object, e As EventArgs) Handles LoginBar.Click
+        LoginForm.ShowDialog()
+
+    End Sub
+
+    Private Sub LogoutBar_Click(sender As Object, e As EventArgs) Handles LogoutBar.Click
+        MatikanSemuaMenu()
+
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        'Jika user login adalah Admin
+        If (admin_role = True) Then
+            MenuAdminBar.Visible = True
+            LoginBar.Visible = False
+            LogoutBar.Visible = True
+        End If
+
+        'Jika user login adalah Pelanggan
+        If (datapengguna_role = True) Then
+            MenuAdminBar.Visible = True
+            LoginBar.Visible = False
+            LogoutBar.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub ExitBar_Click(sender As Object, e As EventArgs) Handles ExitBar.Click
+        End
+    End Sub
+
+    Private Sub MenuAdminBar_Click(sender As Object, e As EventArgs) Handles MenuAdminBar.Click
+
+    End Sub
+
+    Private Sub MenuDosenBar_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
---
+++ Praktikum 15 Tugas Besar/AppMultiLogin/AppMultiLogin/MyMod.vb
@@ -0,0 +1,14 @@
+﻿Module MyMod
+    Public api_folder As String = "appakademik"
+    Public users_api As String = "http://f0833876.xsph.ru/" & api_folder & "/users_api.php"
+    Public datapengguna_api As String = "http://f0833876.xsph.ru/" & api_folder & "/datapengguna_api.php"
+    Public Dashboard As New Form1
+    Public FormDatapengguna As New Datapengguna
+    Public LoginForm As New Login
+    Public datapengguna_baru As Boolean = False
+    Public user_baru As Boolean = False
+    Public admin_role As Boolean = False
+    Public datapengguna_role As Boolean = False
+    Public login_valid As Boolean = False
+
+End Module
